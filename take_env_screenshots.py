f6a0ffde3c5a6d69d073b49f16aa2719c6736184
carla-garcia-medina <carla.garia.medina@outlook.com>
2022-10-24 14:17:27 -0400

    Change logic to have a set number of images per dataset.

--- take_env_screenshots.py
+++ take_env_screenshots.py
@@ -6,7 +6,7 @@ import shutil
 import numpy as np
 
 
-def get_dataset(env_name, runs):
+def get_dataset(env_name, runs=1, num_screenshots=1000):
     env = gym.make(env_name, observation_keys=("glyphs", "pixel", "message"))
     np.set_printoptions(threshold = np.inf)
     
@@ -21,25 +21,23 @@ def get_dataset(env_name, runs):
         glyphs_file = open(out_path + 'glyphs.txt', "w")
         messages_file = open(out_path + 'messages.txt', "w")
 
-        while True:
+        for counter in range(num_screenshots):
             action = env.action_space.sample()
             obs, rewards, done, info = env.step(action)
             if done:
-                break
+                env.reset()
             img_out_path = out_path + '/{0}.jpg'.format(counter)
             imageio.imwrite(img_out_path, obs['pixel'])
             glyphs_file.write(np.array2string(obs['glyphs'],  max_line_width = np.inf)+'\n\n')
             messages_file.write(np.array2string(obs['message'],  max_line_width = np.inf)+'\n\n')
-
-            counter += 1
 
         glyphs_file.close()
         messages_file.close()
 
 
 def main():
-    get_dataset('MiniHack-River-Monster-v0', 5)
-    get_dataset('MiniHack-River-v0', 5)
+    get_dataset('MiniHack-River-Monster-v0')
+    get_dataset('MiniHack-River-v0')
     
 
 if __name__ == '__main__':
